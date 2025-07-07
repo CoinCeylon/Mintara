e73f1327229844d497dcd81ed2c21aae8705526d
SaTh <Sathsara2021@gmail.com>
2025-07-08 04:12:23 +0530

    add: main menu

--- game-app/Scripts/Intro/intro.gd
+++ game-app/Scripts/Intro/intro.gd
@@ -2,4 +2,4 @@ extends VideoStreamPlayer
 
 
 func _on_finished() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/main_level.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
